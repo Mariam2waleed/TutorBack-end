4ef7d1ef202a0498e2f0246a261593d22bc9e37b
Mariam2Waleed <Mariam2waleed@gmail.com>
2023-06-19 19:34:40 +0300

    1

--- scr/models/post.js
+++ scr/models/post.js
@@ -1,11 +1,24 @@
-const mongoose = require('mongoose');
-
-
-const post = mongoose.Schema({
-  name: {type : String, required: true},
-  price : Number,
-  desc : String
-})
-
-module.exports = mongoose.model('POST', post);
-
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const PostSchema = Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    image_path: {
+        type: String,
+        required: true,
+    },
+    body : {
+        type: String,
+        required: true,
+    },
+    author_id: {
+        type: Schema.Types.ObjectId,
+        ref: "Teacher",
+    },
+    date: {
+        type: Date,
+    },
+});
+module.exports = mongoose.model("Post", PostSchema);
